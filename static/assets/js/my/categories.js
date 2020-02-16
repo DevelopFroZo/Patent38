db5192b536f744a90553493ac9f2964ec385eae9
FroZo <frozo02@yandex.ru>
2020-02-16 14:30:44 +0800

    [B] Redesign

--- static/assets/js/my/categories.js
+++ static/assets/js/my/categories.js
@@ -7,59 +7,34 @@ async function fetchCategories(){
   return categories;
 }
 
-function addCategoryToDOM( category ){
-  const categoryList = document.querySelector( "#categoryList" );
-
-  const li = document.createElement( "li" );
-  const cont = document.createElement( "div" );
-  const preview = document.createElement( "div" );
-  const edit = document.createElement( "input" );
-
-  preview.classList.add( "preview" );
-  preview.innerHTML = category.name;
-  preview.addEventListener( "click", () => showEdit( preview, edit ) );
-  cont.appendChild( preview );
-
-  edit.setAttribute( "type", "text" );
-  edit.setAttribute( "cid", category.id );
-  edit.classList.add( "edit" );
-  edit.classList.add( "hidden" );
-  edit.addEventListener( "keydown", ( e ) => editSaveHandler( e, preview, edit ) );
-  edit.addEventListener( "blur", () => editBlurHandler( preview, edit ) );
-  cont.appendChild( edit );
-
-  li.appendChild( cont );
-
-  categoryList.appendChild( li );
-}
-
-function showEdit( preview, edit ){
-  edit.value = preview.innerHTML;
-
-  preview.classList.toggle( "hidden" );
+function showEdit( category, title, edit, toolbarPencil ){
+  category.style.borderBottom = "rgb( 255, 0, 0 ) 1px solid";
+  edit.value = title.innerHTML;
+  title.classList.toggle( "hidden" );
   edit.classList.toggle( "hidden" );
-
   edit.focus();
+  toolbarPencil.classList.toggle( "hidden" );
 }
 
-function hideEdit( preview, edit ){
-  preview.innerHTML = edit.value;
-
+function hideEdit( category, title, edit, toolbarPencil ){
+  category.style.borderBottom = "#6F6F6F 1px solid";
+  title.innerHTML = edit.value;
   edit.classList.toggle( "hidden" );
-  preview.classList.toggle( "hidden" );
+  title.classList.toggle( "hidden" );
+  toolbarPencil.classList.toggle( "hidden" );
 }
 
-async function editSaveHandler( e, preview, edit ){
+async function editSaveHandler( e, category, title, edit, toolbarPencil ){
   if( e.key !== "Enter" ) return;
 
   let name = edit.value;
 
   if( name === "" ){
-    edit.value = preview.innerHTML;
-    name = preview.innerHTML;
+    edit.value = title.innerHTML;
+    name = title.innerHTML;
   }
 
-  if( preview.innerHTML !== name ){
+  if( title.innerHTML !== name ){
     const cid = edit.getAttribute( "cid" );
 
     await fetch( `/api/categories/${cid}`, {
@@ -71,55 +46,170 @@ async function editSaveHandler( e, preview, edit ){
     } );
   }
 
-  hideEdit( preview, edit );
+  hideEdit( category, title, edit, toolbarPencil );
 }
 
-function editBlurHandler( preview, edit ){
+function editBlurHandler( category, title, edit, toolbarPencil ){
   if(
-    preview.innerHTML !== edit.value ||
+    title.innerHTML !== edit.value ||
     edit.classList.contains( "hidden" )
   ) return;
 
-  hideEdit( preview, edit );
+  hideEdit( category, title, edit, toolbarPencil );
 }
 
-async function createCategory(){
-  const addInput = document.querySelector( "#addInput" );
-  const name = addInput.value;
+async function deleteHandler( category, edit ){
+  let confirmed = confirm( "Удалить категорию?" );
 
-  if( name === "" ) return;
+  if( !confirmed ) return;
 
-  const response = await fetch( "/api/categories", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify( { name } )
+  const cid = edit.getAttribute( "cid" );
+
+  const response = await fetch( `/api/categories/${cid}`, {
+    method: "DELETE"
   } );
+
   const jsn = await response.json();
+  const categories = document.querySelector( "#categories" );
 
-  if( !jsn.ok ) return alert( `Ошибка: ${jsn.message}` );
+  if( !jsn.ok ){
+    alert( `Ошибка. ${jsn.message}` );
 
-  addInput.value = "";
+    return;
+  }
 
-  addCategoryToDOM( {
-    id: jsn.data,
-    name
-  } );
+  categories.removeChild( category );
 }
 
-function addInputKeyDownHandler( e ){
-  if( e.key !== "Enter" ) return;
+function addCategoryToDOM( category ){
+  const categories = document.querySelector( "#categories" );
 
-  createCategory();
+  const category_ = document.createElement( "div" );
+  const title = document.createElement( "div" );
+  const edit = document.createElement( "input" );
+  const toolbarPencil = document.createElement( "div" );
+  const toolbarCross = document.createElement( "div" );
+
+  category_.classList.add( "category" );
+
+  title.classList.add( "title" );
+  title.innerHTML = category.name;
+  category_.appendChild( title );
+
+  edit.setAttribute( "type", "text" );
+  edit.setAttribute( "cid", category.id );
+  edit.classList.add( "edit" );
+  edit.classList.add( "hidden" );
+  edit.addEventListener( "keydown", ( e ) => editSaveHandler( e, category_, title, edit, toolbarPencil ) );
+  edit.addEventListener( "blur", () => editBlurHandler( category_, title, edit, toolbarPencil ) );
+  category_.appendChild( edit );
+
+  toolbarPencil.setAttribute( "class", "toolbar-item" );
+  toolbarPencil.addEventListener( "click", () => showEdit( category_, title, edit, toolbarPencil ) );
+  category_.appendChild( toolbarPencil );
+
+  let toolbarImg = document.createElement( "img" );
+  toolbarImg.setAttribute( "src", "assets/img/pencil-light.png" );
+  toolbarPencil.appendChild( toolbarImg );
+
+  toolbarCross.setAttribute( "class", "toolbar-item" );
+  toolbarCross.addEventListener( "click", () => deleteHandler( category_, edit ) );
+  category_.appendChild( toolbarCross );
+
+  toolbarImg = document.createElement( "img" );
+  toolbarImg.setAttribute( "src", "assets/img/cross-light.png" );
+  toolbarCross.appendChild( toolbarImg );
+
+  categories.appendChild( category_ );
+}
+
+// async function createCategory(){
+//   const addInput = document.querySelector( "#addInput" );
+//   const name = addInput.value;
+//
+//   if( name === "" ) return;
+//
+//   const response = await fetch( "/api/categories", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json"
+//     },
+//     body: JSON.stringify( { name } )
+//   } );
+//   const jsn = await response.json();
+//
+//   if( !jsn.ok ) return alert( `Ошибка: ${jsn.message}` );
+//
+//   addInput.value = "";
+//
+//   addCategoryToDOM( {
+//     id: jsn.data,
+//     name
+//   } );
+// }
+//
+// function addInputKeyDownHandler( e ){
+//   if( e.key !== "Enter" ) return;
+//
+//   createCategory();
+// }
+
+function addCategoryShow(){
+  const addCategory = document.querySelector( "#addCategory" );
+
+  if( !addCategory.classList.contains( "hidden" ) ) return;
+
+  const addCategoryInput = document.querySelector( "#addCategoryInput" );
+
+  addCategoryInput.value = "";
+  addCategory.classList.toggle( "hidden" );
+  addCategoryInput.focus();
+}
+
+function addCategoryHide(){
+  document.querySelector( "#addCategory" ).classList.add( "hidden" );
+}
+
+async function createCategory( e ){
+  if( e.key === "Escape" ) return addCategoryHide();
+  else if( e.key !== "Enter" ) return;
+
+  const name = e.target.value;
+
+  if( name !== "" ){
+    const response = await fetch( "/api/categories", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify( { name } )
+    } );
+    const jsn = await response.json();
+
+    if( !jsn.ok ) return alert( `Ошибка. ${jsn.message}` );
+
+    addCategoryHide();
+    addCategoryToDOM( {
+      id: jsn.data,
+      name
+    } );
+  }
+}
+
+function addCategoryBlurHandler( e ){
+  if( e.target.value === "" ) addCategoryHide();
 }
 
 async function index(){
   const categories = await fetchCategories();
+  const addCategoryInput = document.querySelector( "#addCategoryInput" );
 
   categories.forEach( addCategoryToDOM );
-  document.querySelector( "#addInput" ).addEventListener( "keydown", addInputKeyDownHandler );
-  document.querySelector( "#addButton" ).addEventListener( "click", createCategory );
+  // document.querySelector( "#addInput" ).addEventListener( "keydown", addInputKeyDownHandler );
+  // document.querySelector( "#addButton" ).addEventListener( "click", createCategory );
+  document.querySelector( "#newCategory" ).addEventListener( "click", addCategoryShow );
+  addCategoryInput.addEventListener( "keydown", createCategory );
+  addCategoryInput.addEventListener( "blur", addCategoryBlurHandler );
 }
 
 window.addEventListener( "load", index );
